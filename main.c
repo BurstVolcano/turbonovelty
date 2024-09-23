6b0935b95e53476c6a313dbfbdccd8d811d7d87e
BurstVolcano <kakwi9-vitjir-hiwMyp>
2024-09-23 21:29:29 +0530

    Just trying

--- main.c
+++ main.c
@@ -9,5 +9,11 @@ int main(void)
     *If a file is added, it is untracked
     *If a file is added, it is staged
     *If a file is comitted, it is unchanged
+    *
+    *git add <filename> is used to stage
+    *if there are lots of files, use git add .
+    *git comit -m "Some comment" is used to commit
+    *
+    *git push origin main is used to copy the code written on local computer into git repository
     */
 }
