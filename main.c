4f38d6f72d8e3a38676a3546f6121c0dc940d426
BurstVolcano <tubs-mousers-0m@icloud.com>
2024-09-23 19:39:55 +0530

    Just trying out

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+   /* git version is checked by git --version
+    * git clone <somelink> is used for cloning a repository on our local machine
+    *cd is used to change directory
+    *If a file is edited, it is modified,
+    *If a file is added, it is untracked
+    *If a file is added, it is staged
+    *If a file is comitted, it is unchanged
+    */
+}
